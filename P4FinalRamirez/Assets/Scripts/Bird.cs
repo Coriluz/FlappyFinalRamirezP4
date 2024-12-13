2221ee7975709a663a2959de0ea77c53dbebf5d1
Coriluz <760351@student.dusd.net>
2024-12-13 11:46:40 -0800

    The bugs were fixed
    I added the columns. The video 8/10 ended on 9:31.

--- P4FinalRamirez/Assets/Scripts/Bird.cs
+++ P4FinalRamirez/Assets/Scripts/Bird.cs
@@ -11,8 +11,8 @@ public class Bird : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator> ();
     }
 
     // Update is called once per frame
